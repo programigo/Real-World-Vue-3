0821c635b3fccdfb8c24f9a979210fc697595399
Krasimir <iznagikz@gmail.com>
2022-06-11 23:13:46 +0300

    Initial commit.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <nav>
-    <router-link :to="{ name: 'EventList' }">Events</router-link> |
-    <router-link :to="{ name: 'About' }">About</router-link>
-  </nav>
-  <router-view />
+  <div id="app">
+    <div id="flashMessage" v-if="GStore.flashMessage">
+      {{ GStore.flashMessage }}
+    </div>
+    <nav>
+      <router-link :to="{ name: 'EventList' }">Events</router-link> |
+      <router-link :to="{ name: 'About' }">About</router-link>
+    </nav>
+    <router-view />
+  </div>
 </template>
+
+<script>
+export default {
+   inject: ['GStore'],
+}
+</script>
 
 <style>
 #app {
@@ -30,5 +41,19 @@ nav a.router-link-exact-active {
 
 h4 {
   font-size: 20px;
+}
+
+@keyframes yellowfade {
+  from {
+    background: yellow;
+  }
+  to {
+    background: transparent;
+  }
+}
+
+#flashMessage {
+  animation-name: yellowfade;
+  animation-duration: 3s;
 }
 </style>
